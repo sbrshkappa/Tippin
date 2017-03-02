c5c6739d7dd704d84a2bd11bd89783c6e73b0061
Sabareesh Kappagantu <sabareesh@sabareeshs-mbp.lan>
2017-03-01 21:42:27 -0800

    Initial Commit

---
+++ tippin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippin
+//
+//  Created by Sabareesh Kappagantu on 3/1/17.
+//  Copyright © 2017 Sabareesh Kappagantu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
