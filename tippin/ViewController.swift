65a01a7fcfbd82f3605f47b46169fca148cf68b4
Sabareesh Kappagantu <sabareesh@Sabareeshs-MBP.lan>
2017-03-04 10:19:59 -0800

    Added functionality to split the bill value. Still needs to be fixed.

--- tippin/ViewController.swift
+++ tippin/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billValue: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var splitAmountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Setting the cursor to show up by default in the Bill Value Field
+        billValue.becomeFirstResponder()
+        billValue.placeholder = "0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,21 +32,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
-
+    
+    
     @IBAction func calculateTip(_ sender: AnyObject) {
         
         let tipPercentages = [0.10, 0.15, 0.18, 0.20]
         
         //Change the bill value to number else set default to 0
-        let bill = Double(billField.text!) ?? 0
+        let bill = Double(billValue.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        let splitValue = Double(splitLabel.text!) ?? 0
+        let splitAmount = total / splitValue
+        
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        splitAmountLabel.text = String(format: "$%.2f", splitAmount)
+    }
+    
+    
+    @IBAction func incrementSplit(_ sender: UIStepper) {
+        splitLabel.text = "\(Int(sender.value))"
     }
 }
 
